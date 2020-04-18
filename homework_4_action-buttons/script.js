2637a50485b540f9fa0ef06ba5d09782ee747c67
NadezhdaBabenko <chestnaya.n@gmail.com>
2020-04-18 15:35:28 +0200

    add changes from Anna

--- homework_4_action-buttons/script.js
+++ homework_4_action-buttons/script.js
@@ -1,13 +1,7 @@
 let mainButton = document.getElementById("mainButton");
 const mainText = "Interesting button";
-
 function clickChange() {
-    if (mainButton.classList.contains('bigger')) {
-        mainButton.classList.remove('bigger');
-    }
-    else {
-        mainButton.classList.add('bigger');
-    };
+    mainButton.classList.toggle("bigger");
 }
 
 function mouseOver(event) {
@@ -23,12 +17,9 @@ function mouseLeave(event) {
 mainButton.addEventListener("click", clickChange);
 mainButton.addEventListener("mouseover", mouseOver);
 mainButton.addEventListener("mouseleave", mouseLeave);
-
 // 2 button
-
 let bonusButton = document.getElementById("bonusButton");
 let hiddenButton = document.getElementById("hiddenButton");
-
 function bonusMouseOver(event) {
     hiddenButton.style.display = "block";
 }
@@ -39,17 +30,9 @@ function bonusMouseLeave(event) {
 
 bonusButton.addEventListener("mouseover", bonusMouseOver);
 bonusButton.addEventListener("mouseleave", bonusMouseLeave);
-
 // 3 button
 let tooltipButton = document.getElementById("tooltipButton");
-let lastBlock = document.getElementById("lastBlock");
-
-const ttBox = document.createElement("div");
-ttBox.id = "tooltip";
-ttBox.classList.add('tooltip');
-ttBox.style.visibility = "hidden";
-lastBlock.appendChild(ttBox);
-
+let ttBox = document.getElementById("tooltip");
 const ttTurnOn = ((evt) => {
     const boundBox = evt.target.getBoundingClientRect();
     const coordX = boundBox.left;
@@ -67,7 +50,7 @@ const ttTurnOff = (() => { ttBox.style.visibility = "hidden"; });
 
 tooltipButton.addEventListener("mouseover", ttTurnOn , false);
 tooltipButton.addEventListener("mouseout", ttTurnOff , false);
-document.getElementById("tooltip") . addEventListener("click", ttTurnOff , false);
+ttBox.addEventListener("click", ttTurnOff , false);
 
 
 
